976654df88257913a60e6a4835353f7aa0583207
PAGGALA241 <vvardhan200@gmail.com>
2023-03-26 18:45:41 +0000

    Card

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+    // Write your code here.
+    <div class="BgContainer">
+        <h1 class="MainHeading">Congratulations</h1>
+        <div class="CardContainer">
+            <img class="img" src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"/>
+            <h2 class="heading">Kiran V</h2>
+            <p>Vishnu Institute of Computer Education and Technology, Bhimavaram</p>
+            <img class="img" src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"/>
+
+        </div>
+    </div>
+  )
+  
+ReactDOM.render(element, document.getElementById('root'))
